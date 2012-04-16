21ca33fef16e8ffa407cce3502aa38f5bba31048
Ran Tavory <rantav@gmail.com>
2012-04-16 23:13:39 +0300

    Add the rest_uri option

--- lib/glu_client.rb
+++ lib/glu_client.rb
@@ -3,8 +3,16 @@ require "glu_client/version"
 module GluClient
   class Api
     attr_accessor :fabric
+    attr_accessor :rest_uri
+
+    #
+    # options:
+    # fabric: fabric name
+    # rest_uri: for example 'http://user:pass@glu.com:8080/console/rest/v1/'
+    #
     def initialize(options)
       @fabric = options[:fabric]
+      @rest_uri = options[:rest_uri]
     end
   end
 end
--- spec/glu_client_spec.rb
+++ spec/glu_client_spec.rb
@@ -1,7 +1,12 @@
 require 'glu_client'
 
 describe GluClient::Api do
-  it "parses the fabric from the yaml file" do
-    GluClient::Api.new(:fabric => 'fabric_name').fabric.should == 'fabric_name'
+  describe "initialize" do
+    it "accepts the fabric" do
+      GluClient::Api.new(:fabric => 'fabric_name').fabric.should == 'fabric_name'
+    end
+    it "accepts the rest_uri" do
+      GluClient::Api.new(:rest_uri => 'https://glu.com:8080').rest_uri.should == 'https://glu.com:8080'
+    end
   end
 end
