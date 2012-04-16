45ae4401b04b6aaae9dbad4e7e1c8b7c2cc28b88
Ran Tavory <rantav@gmail.com>
2012-04-16 23:28:09 +0300

    Add the method get_model_live

--- lib/glu_client.rb
+++ lib/glu_client.rb
@@ -1,4 +1,6 @@
 require "glu_client/version"
+require 'rest_client'
+require 'json'
 
 module GluClient
   class Api
@@ -13,6 +15,16 @@ module GluClient
     def initialize(options)
       @fabric = options[:fabric]
       @rest_uri = options[:rest_uri]
+      @api_uri = "#{@rest_uri}#{@fabric}"
+    end
+
+    def get_model_live
+      get_json "/model/live"
+    end
+
+    def get_json(resource)
+      response = RestClient.get @api_uri + resource
+      json = JSON.parse(response)
     end
   end
 end
--- spec/glu_client_spec.rb
+++ spec/glu_client_spec.rb
@@ -9,4 +9,14 @@ describe GluClient::Api do
       GluClient::Api.new(:rest_uri => 'https://glu.com:8080').rest_uri.should == 'https://glu.com:8080'
     end
   end
+
+  describe "get_model_live" do
+    before :each do
+      @json = '{}'
+      RestClient.stub(:get).and_return(@json)
+    end
+    it 'parses the json and returns it (empty json)' do
+      GluClient::Api.new({}).get_model_live.should == {}
+    end
+  end
 end
